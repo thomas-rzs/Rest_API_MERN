3437ef6195f6425bf333d5c05ed8b4f703a80816
thomas-rzs <thomas.rouzes@shayp.com>
2022-06-09 18:23:05 +0200

    Sensors controller and routes setup

--- backend/controller/sensorsController.js
+++ backend/controller/sensorsController.js
@@ -1,14 +1,17 @@
+const asyncHandler = require('express-async-handler')
+
+
 const sensors = [
     {id:1, type:'alpha', datas:[1,2,3]},
     {id:2, type:'beta', datas:[1,2,3], metrics:false},
     {id:3, type:'omega', datas:{a:1,b:2}},
 ]
 
-const getSensors = (req, res) => {
+const getSensors = asyncHandler(async(req, res) => {
     res.status(200).json({message: 'Get sensors'})
-}
+})
 
-const getSensorsId = (req, res) => {
+const getSensorsId = asyncHandler(async(req, res) => {
     //check if exist
     for (var i = 0; i < sensors.length; i++){
         //get object
@@ -16,9 +19,9 @@ const getSensorsId = (req, res) => {
     res.status(200).json({sensor: `${sensors[req.params.id-1].id}`,
                           type: `${sensors[req.params.id-1].type}`,
                           datas: `${sensors[req.params.id-1].datas}`})
-}
+})
 
-const setSensors = (req, res) => {
+const setSensors = asyncHandler(async(req, res) => {
     if (!req.body.id){
         res.status(400)
         throw new Error('Add a id')
@@ -27,18 +30,18 @@ const setSensors = (req, res) => {
     sen = {id:req.body.id, type:req.body.type, datas:req.body.datas}
     sensors.push(sen)
     res.status(201).json({message: `Added sensor`})
-}
+})
 
-const updateSensor = (req, res) => {
+const updateSensor = asyncHandler(async(req, res) => {
     //update
     res.status(200).json({message: `Update sensor ${req.params.id}`})
-}
+})
 
-const deleteSensors = (req, res) => {
+const deleteSensors = asyncHandler(async(req, res) => {
     //check if exist
     sensors.splice(req.params.id-1,req.params.id-1)
     res.status(200).json({message: `Deleted sensor ${req.params.id}`})
-}
+})
 
 module.exports = {
     getSensors,
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/sensors', require('./routes/sensorRoutes'))
+app.use('/api/sensors', require('./routes/sensorRoutes'))
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server start on port ${port}`))
